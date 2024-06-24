03715d69b23fc9c98e11612a60fd6ce9314ebc9e
PythonMonty <andrewnovosel90@gmail.com>
2024-06-24 16:48:30 -0400

    Added a separate function to change the color

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
+// Spacing
 const spacingSlider = document.getElementById("spacing");
-const grayscaleSlider = document.getElementById("grayscale");
-
-function updateBaseColor() {
-  const newColor = document.getElementById("base").value;
-  document.documentElement.style.setProperty("--base", newColor);
-}
 
 function updateSpacing() {
   const spacingValue = document.getElementById("spacing").value + "px";
   document.documentElement.style.setProperty("--spacing", spacingValue);
 }
 
+spacingSlider.addEventListener("change", updateSpacing);
+spacingSlider.addEventListener("mousemove", updateSpacing);
+
+// Grayscale
+const grayscaleSlider = document.getElementById("grayscale");
+
 function updateGrayscale() {
   const grayscaleLevel = document.getElementById("grayscale").value + "%";
   document.documentElement.style.setProperty("--grayscale", grayscaleLevel);
 }
 
-const colorPicker = document.getElementById("base");
-colorPicker.addEventListener("change", updateBaseColor);
-
-spacingSlider.addEventListener("change", updateSpacing);
-spacingSlider.addEventListener("mousemove", updateSpacing);
-
 grayscaleSlider.addEventListener("change", updateGrayscale);
 grayscaleSlider.addEventListener("mousemove", updateGrayscale);
+
+// Color Picker
+const colorPicker = document.getElementById("base");
+
+function updateBaseColor() {
+  const newColor = document.getElementById("base").value;
+  document.documentElement.style.setProperty("--base", newColor);
+}
+
+colorPicker.addEventListener("change", updateBaseColor);
 
 // const inputs = document.querySelectorAll(".controls input");
 // function handleUpdate() {
